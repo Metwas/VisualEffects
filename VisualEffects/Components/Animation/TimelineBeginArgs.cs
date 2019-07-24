c4720de4481ce8c19f48037522564818fe053a8a
Metwas <metwas.seup@gmail.com>
2019-07-24 10:52:17 +0100

    Changed the framerate type from int to an unsigned int

--- VisualEffects/Components/Animation/TimelineBeginArgs.cs
+++ VisualEffects/Components/Animation/TimelineBeginArgs.cs
@@ -15,7 +15,7 @@ namespace VisualEffects.Components.Animation
 		/// <param name="frameRate"The desired framerate></param>
 		/// <param name="duration">The duration until completion</param>
 		/// <param name="elapsedTime">The current total elapsed time</param>
-		public TimelineBeginArgs(int frameRate, double duration, TimeSpan elapsedTime)
+		public TimelineBeginArgs(uint frameRate, double duration, TimeSpan elapsedTime)
 		{
 			this.Duration = duration;
 			this.FrameRate = frameRate;
@@ -28,7 +28,7 @@ namespace VisualEffects.Components.Animation
 		/// <param name="frameRate"The desired framerate></param>
 		/// <param name="duration">The duration until completion</param>
 		/// <param name="elapsedTime">The current total elapsed time in milliseconds</param>
-		public TimelineBeginArgs(int frameRate, double duration, double elapsedMilliseconds)
+		public TimelineBeginArgs(uint frameRate, double duration, double elapsedMilliseconds)
 			: this(frameRate, duration, TimeSpan.FromMilliseconds(elapsedMilliseconds))
 		{
 		}
@@ -38,7 +38,7 @@ namespace VisualEffects.Components.Animation
 		/// <summary>
 		/// The framerate for a given <see cref="Timeline"/>
 		/// </summary>
-		public int FrameRate { get; }
+		public uint FrameRate { get; }
 
 		/// <summary>
 		/// The duration for a given <see cref="Timeline"/>
